a7ea0c060edf0e38beec65d1ed1a6a95a387eed6
Ben-Cariello <bcariell@uncc.edu>
2024-09-08 23:37:57 -0400

    User Login

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,14 +1,32 @@
 from pydoc import describe
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Room, Topic
 from .forms import RoomForm
 from django.db.models import Q
+from django.contrib.auth.models import User
 # Create your views here.
 #rooms = [
 #   {'id':1, 'name':'Lets learn python!'},
 #  {'id':2, 'name':'Design with me'},
 # {'id':3, 'name':'Frontend developers'},
 #]
+
+
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except: 
+            messages.error(request, "User does not exist")
+            
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
